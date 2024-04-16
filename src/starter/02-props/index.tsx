1fddade4425d80ffac0d8ebfa8dc188d28fafab5
serhatbek <serhat.bekk@gmail.com>
2024-04-16 13:42:13 +0300

    Props - Type Alias / Interface

--- src/starter/02-props/index.tsx
+++ src/starter/02-props/index.tsx
@@ -1,4 +1,9 @@
-const Component = ({ name, age }: { name: string; age: number }) => {
+interface ComponentProps {
+  name: string;
+  age: number;
+}
+
+const Component = ({ name, age }: ComponentProps) => {
   return (
     <div>
       <h2>{name}</h2>
@@ -8,3 +13,14 @@ const Component = ({ name, age }: { name: string; age: number }) => {
 };
 
 export default Component;
+
+// const Component = (props: ComponentProps) => {  //NOTE - We can also directly reference props with types
+//   return (
+//     <div>
+//       <h2>{props.name}</h2>
+//       <h2>{props.age}</h2>
+//     </div>
+//   );
+// };
+
+// export default Component;
