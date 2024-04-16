2416b12f2a3b2150094d20c7752c1461c45fae6e
serhatbek <serhat.bekk@gmail.com>
2024-04-16 15:12:21 +0300

    State - Custom List

--- src/starter/03-state/index.tsx
+++ src/starter/03-state/index.tsx
@@ -1,10 +1,36 @@
 import { useState } from 'react';
 
+interface Link {
+  id: number;
+  url: string;
+  text: string;
+}
+
+const navLinks = [
+  {
+    id: 1,
+    url: '/',
+    text: 'Home',
+  },
+  {
+    id: 2,
+    url: '/',
+    text: 'Products',
+  },
+  {
+    id: 3,
+    url: '/',
+    text: 'About',
+  },
+];
+
 const Component = () => {
   const [text, setText] = useState('Unicorn');
   const [number, setNumber] = useState(6);
   const [list, setList] = useState<string[]>([]);
+  const [links, setLinks] = useState<Link[]>(navLinks);
 
+  console.log(links);
   return (
     <div>
       <h2 className='mb-1'>React State & Typescript</h2>
@@ -14,6 +40,7 @@ const Component = () => {
           setText('Horses in my dreams');
           setNumber(number * 2);
           setList(['meow', 'cik', 'tıs']);
+          setLinks([...links, { id: 3, url: '/', text: 'Contact' }]);
         }}
       >
         click me
