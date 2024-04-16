cd6f33098f1bf99c48bc8c1834ea0affa9cb76f0
serhatbek <serhat.bekk@gmail.com>
2024-04-17 00:07:25 +0300

    Events - Form Event

--- src/starter/04-events/index.tsx
+++ src/starter/04-events/index.tsx
@@ -1,16 +1,37 @@
 import { useState } from 'react';
 
+interface Person {
+  text: string;
+  email: string;
+}
+
 const Component = () => {
   const [text, setText] = useState('');
   const [email, setEmail] = useState('');
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //NOTE - If we setup functions as references, we need to provide correct type for the event and correct HTML element.
     setEmail(e.target.value);
   };
 
-  const handleSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    //NOTE - If we setup functions as references, we need to provide correct type for the event and correct HTML element.
     e.preventDefault();
-    console.log(email);
+    const formData = new FormData(e.currentTarget); //NOTE - To use FormData, we need to give names to form elements (name, email, password...)
+    //_____------- OR -----______
+    // const formData = new FormData(e.target as HTMLFormElement); //NOTE - Wee need a type assertion with FormData, this tells TypeScript to treat e.target as an HTMLFormElement object.
+    // const text = formData.get('text') as string;    //NOTE - formData.get() get from data one by one.
+    // const email = formData.get('email') as string;
+    // const data: Person = {
+    //   text,
+    //   email,
+    // };
+    //_____------- OR -----______
+    const data = Object.fromEntries(formData); //NOTE - Object.fromEntries() gets all the form data at once.
+    const person: Person = {
+      name: data.name as string,
+      email: data.email as string,
+    };
   };
 
   return (
@@ -21,7 +42,8 @@ const Component = () => {
           type='text'
           className='form-input mb-1'
           value={text}
-          onChange={(e) => setText(e.target.value)}
+          onChange={(e) => setText(e.target.value)} //NOTE - If we use event in callback inline function, typescript knows that it is event.
+          name='text'
         />
 
         <input
@@ -29,6 +51,7 @@ const Component = () => {
           className='form-input mb-1'
           value={email}
           onChange={handleChange}
+          name='email'
         />
         <button type='submit' className='btn btn-block'>
           submit
