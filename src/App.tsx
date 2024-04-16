0534113724977e9d6442f0c959fd6d48db739e5c
serhatbek <serhat.bekk@gmail.com>
2024-04-16 14:59:17 +0300

    State

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import Component from './starter/02-props/index';
+import Component from './starter/03-state/index';
 
 function App() {
   return (
     <main>
       <h2>React & Typescript</h2>
-      <Component name='peter' age={28} />
-      <Component name='peter' age={28}>
-        <h2>Hello World</h2>
-      </Component>
+      <Component />
     </main>
   );
 }
--- src/starter/03-state/index.tsx
+++ src/starter/03-state/index.tsx
@@ -1,5 +1,28 @@
+import { useState } from 'react';
+
 const Component = () => {
-  return <div>Component 3</div>;
+  const [text, setText] = useState('Unicorn');
+  const [number, setNumber] = useState(6);
+  const [list, setList] = useState<string[]>([]);
+
+  return (
+    <div>
+      <h2 className='mb-1'>React State & Typescript</h2>
+      <button
+        className='btn btn-center'
+        onClick={() => {
+          setText('Horses in my dreams');
+          setNumber(number * 2);
+          setList(['meow', 'cik', 'tıs']);
+        }}
+      >
+        click me
+      </button>
+      <p>{text}</p>
+      <p>{number}</p>
+      <p>{list}</p>
+    </div>
+  );
 };
 
 export default Component;
