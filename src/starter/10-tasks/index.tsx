f38d3c088d1c5cd72e872b06d8161456813fa27c
serhatbek <serhat.bekk@gmail.com>
2024-04-19 01:46:57 +0300

    Tasks Project - Setup

--- src/starter/10-tasks/index.tsx
+++ src/starter/10-tasks/index.tsx
@@ -1,5 +1,19 @@
+import { useState } from 'react';
+import { type Task } from './types';
+import Form from './Form';
+import List from './List';
+
 const Component = () => {
-  return <div>Component 10</div>;
+  const [tasks, setTasks] = useState<Task[]>([]);
+  return (
+    <div>
+      <h2>Tasks</h2>
+      <div>
+        <Form />
+        <List />
+      </div>
+    </div>
+  );
 };
 
 export default Component;
