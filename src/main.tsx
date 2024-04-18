ab3b356a8eb1b1f0707e0e8dff3c947a075d8766
serhatbek <serhat.bekk@gmail.com>
2024-04-18 23:36:07 +0300

    RTK - Complete

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { store } from './store.tsx';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>
 );
--- src/starter/09-rtk/index.tsx
+++ src/starter/09-rtk/index.tsx
@@ -1,29 +1,30 @@
-import { useDispatch } from 'react-redux';
+import { useAppDispatch, useAppSelector } from '../../hooks';
 import { increase, decrease, reset, setStatus } from './counterSlice';
 
 function Component() {
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
+  const { count, status } = useAppSelector((state) => state.counter);
   return (
     <div>
-      <h2>Count: 0</h2>
-      <h2>Status: Pending</h2>
+      <h2>Count: {count}</h2>
+      <h2>Status: {status}</h2>
 
       <div className='btn-container'>
-        <button onClick={() => dispatch(increase)} className='btn'>
+        <button onClick={() => dispatch(increase())} className='btn'>
           Increment
         </button>
-        <button onClick={() => dispatch(decrease)} className='btn'>
+        <button onClick={() => dispatch(decrease())} className='btn'>
           Decrement
         </button>
-        <button onClick={() => dispatch(reset)} className='btn'>
+        <button onClick={() => dispatch(reset())} className='btn'>
           Reset
         </button>
       </div>
       <div className='btn-container'>
-        <button onClick={() => dispatch(setStatus)} className='btn'>
+        <button onClick={() => dispatch(setStatus('active'))} className='btn'>
           Set Status to Active
         </button>
-        <button className='btn' onClick={dispatch(dispatch(setStatus))}>
+        <button className='btn' onClick={() => dispatch(setStatus('inactive'))}>
           Set Status to Inactive
         </button>
       </div>
