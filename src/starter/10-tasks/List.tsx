8318a33c84b36ca733f23453419730000c94d867
serhatbek <serhat.bekk@gmail.com>
2024-04-20 16:20:06 +0300

    Tasks Project - Local Storage and Delete Task

--- src/starter/10-tasks/List.tsx
+++ src/starter/10-tasks/List.tsx
@@ -3,21 +3,32 @@ import { Task } from './types';
 type ListProps = {
   tasks: Task[];
   toggleTask: ({ id }: { id: string }) => void;
+  deleteTask: (taskId: string) => void;
 };
 
-const List = ({ toggleTask, tasks }: ListProps) => {
+const List = ({ toggleTask, tasks, deleteTask }: ListProps) => {
   return (
-    <ul>
-      {tasks?.map((task) => {
-        const { id, description, isCompleted } = task;
+    <ul className='list'>
+      {tasks.map((task) => {
         return (
-          <li key={id}>
-            {description}
-            <input
-              type='checkbox'
-              onChange={() => toggleTask({ id: task.id })}
-              checked={isCompleted}
-            />
+          <li key={task.id}>
+            <p className='task-text'>{task.description}</p>
+            <div>
+              <input
+                type='checkbox'
+                checked={task.isCompleted}
+                onChange={() => {
+                  toggleTask({ id: task.id });
+                }}
+              />
+              <button
+                style={{ marginLeft: '20px' }}
+                className='btn'
+                onClick={() => deleteTask(task.id)}
+              >
+                Delete
+              </button>
+            </div>
           </li>
         );
       })}
--- src/starter/10-tasks/index.tsx
+++ src/starter/10-tasks/index.tsx
@@ -1,18 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { type Task } from './types';
 import Form from './Form';
 import List from './List';
 
+const loadTasks = (): Task[] => {
+  const storedTasks = localStorage.getItem('tasks');
+  return storedTasks ? JSON.parse(storedTasks) : [];
+};
+
+const updateTasks = (tasks: Task[]): void => {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
 const Component = () => {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>(loadTasks());
 
   console.log('tasks', tasks);
 
-  const addTask = (task: Task) => {
+  const addTask = (task: Task): void => {
     setTasks([...tasks, task]);
   };
 
-  const toggleTask = ({ id }: { id: string }) => {
+  const toggleTask = ({ id }: { id: string }): void => {
     setTasks(
       tasks.map((task) => {
         if (task.id === id) {
@@ -23,12 +32,20 @@ const Component = () => {
     );
   };
 
+  const deleteTask = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+
+  useEffect(() => {
+    updateTasks(tasks);
+  }, [tasks]);
+
   return (
     <section>
       <h2>Tasks</h2>
       <div>
         <Form addTask={addTask} />
-        <List toggleTask={toggleTask} tasks={tasks} />
+        <List toggleTask={toggleTask} tasks={tasks} deleteTask={deleteTask} />
       </div>
     </section>
   );
